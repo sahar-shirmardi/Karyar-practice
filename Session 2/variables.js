ecaabef9253f153b94260d8370f8f6ded282e9fe
sahar-shirmardi <sahar.s.shirmardi@gmail.com>
2021-11-25 18:22:58 +0330

    4 Azar

---
+++ Session 2/variables.js
@@ -0,0 +1,32 @@
+// variables
+
+// 1.Let keyword
+
+let number = 25;
+console.log(number);
+number = 50;
+console.log(number);
+
+// 2.Const keyword
+
+const number2 = 34;
+console.log(number2);
+// number2 = 45;
+// console.log(number2);
+
+// 3.Var keyword
+
+var number3 = 12;
+console.log(number3);
+number3 = 67;
+console.log(number3);
+
+// Naming rules
+
+let theFirstNumber; //camelCased
+let the first number; // Wrong; name cannot contain spaces
+let abcd123$$$___; //letters, numbers, $, _
+let 12name; // Wrong; name cannot begin with number
+let let; // Keywords such as 'let', 'const', 'var', etc
+
+// https://www.w3schools.com/js/js_reserved.asp
